8c47afbf9e5fd6c492c5813386a9e8a78cc6ed94
Baagii <baagii1993@yahoo.com>
2024-11-04 14:22:38 -0800

    added vercel.json file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'CineFlix',  // Adjust this as needed
+  base: '/',  // Adjust this as needed
   build: {
     outDir: 'dist' // Change 'CineFlix' to 'dist'
   },
