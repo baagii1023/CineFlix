1b9d0c7d554514ca875aff81e59aee7ada2f43de
Baagii <baagii1993@yahoo.com>
2024-11-04 14:29:31 -0800

    added vercel.json file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { APP_FOLDER_NAME } from './src/globals/globals'
+
 
 export default defineConfig({
   base: `/${APP_FOLDER_NAME}/`, // Ensure this matches the subdirectory on Vercel
